87890b48be1c1a8fb5bd344af4f61d967a5f0950
trganda <jongsx1203@gmail.com>
2024-11-14 23:25:46 +0800

    temp

--- src/main/java/com/github/trganda/components/dashboard/HostFilterPane.java
+++ src/main/java/com/github/trganda/components/dashboard/HostFilterPane.java
@@ -4,16 +4,23 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+
 import lombok.Getter;
 
 @Getter
 public class HostFilterPane extends JPanel {
-  private JTextField hosTextField;
-  private JTextField statusTextField;
+  private JComboBox<String> hostComboBox;
+  private DefaultComboBoxModel<String> hostComboBoxModel;
+  // private JTextField statusTextField;
 
   public HostFilterPane() {
     this.setupComponents();
@@ -22,11 +29,26 @@ public class HostFilterPane extends JPanel {
   }
 
   private void setupComponents() {
-    hosTextField = new JTextField();
-    hosTextField.setPreferredSize(new Dimension(200, hosTextField.getPreferredSize().height));
+    hostComboBoxModel = new DefaultComboBoxModel<>();
+    hostComboBox = new JComboBox<>(hostComboBoxModel);
+    hostComboBox.setEditable(true);
+    // hidden the button and remove it from the combobox
+    hostComboBox.setUI(new BasicComboBoxUI(){
+      @Override
+      protected JButton createArrowButton() {
+        return new JButton() {
+          @Override
+          public int getWidth() {
+              return 0;
+          }
+      };
+      }
+    });
+    hostComboBox.remove(hostComboBox.getComponent(0));
+    hostComboBox.setPreferredSize(new Dimension(200, hostComboBox.getPreferredSize().height));
 
-    statusTextField = new JTextField();
-    statusTextField.setPreferredSize(new Dimension(200, statusTextField.getPreferredSize().height));
+    // statusTextField = new JTextField();
+    // statusTextField.setPreferredSize(new Dimension(200, statusTextField.getPreferredSize().height));
   }
 
   private void setupLayout() {
@@ -45,7 +67,7 @@ public class HostFilterPane extends JPanel {
     gbc.gridy = 0;
     gbc.insets = new Insets(0, 0, 5, 2);
     gbc.fill = GridBagConstraints.HORIZONTAL;
-    this.add(hosTextField, gbc);
+    this.add(hostComboBox, gbc);
 
     // gbc.gridx = 0;
     // gbc.gridy = 1;
